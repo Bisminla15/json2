9393fba8faf9efa0ecf035e581887ec2313f6542
Bisminla15 <86422951+Bisminla15@users.noreply.github.com>
2021-12-09 09:56:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Json2</title>
 
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 
@@ -33,6 +33,19 @@
         </tbody>
     </table> 
 </div>
+<div id="main2">
+    <table>
+        <thead>
+            <th>postID</th><br/>
+            <th>id</th>
+            <th>name</th>
+            <th>email</th>
+            <th>body</th>
+        </thead>
+        <tbody id="tblPost2">
+        </tbody>
+    </table>
+</div>
 <button id="btnComments"> comment </button>
 <div id="comment">
     <table>
@@ -40,7 +53,7 @@
             <tr>
                 <th> postId </th>
                 <th> ID </th>
-                <th> Name </th>
+                <th> name </th>
                 <th> email </th>
                 <th> body </th>
             </tr>
@@ -54,6 +67,7 @@
     function showDetails(id){
         $("#main").hide();
         $("#detail").show();
+        $("main2").hide();
         var url = "https://jsonplaceholder.typicode.com/posts/"+id;
         $.getJSON(url)
             .done((data)=>{
@@ -72,12 +86,13 @@
     }
     function loadPosts(){
         $("#main").show();
-        $("#details").hide();
+        $("#detail").hide();
+        $("main2").hide();
         var url = "https://jsonplaceholder.typicode.com/posts";
         $.getJSON(url)
             .done((data)=>{
                 $.each(data, (k, item)=>{
-                    console.log(item);
+                    
                     var line = "<tr>";
                         line += "<td>"+ item.id + "</td>";
                         line += "<td><b>"+ item.title + "</b><br/>";
@@ -92,20 +107,44 @@
 
             })
     }
-    function showComments(id){
+    function loadPosts2(){
+        $("#main").show();
+        $("#detail").hide();
+        $("#comment").hide();
+        $("main2").hide();
+        var url = "https://jsonplaceholder.typicode.com/comments";
+        $.getJSON(url)
+            .done((data)=>{
+                $.each(data, (k, item)=>{
+                    var line = "<tr>";
+                        line += "<td>" + item.postId + "</td>"
+                        line += "<td><b>" + item.id + "</b><br/>"
+                        line += "<td><b>" + item.name+ "</b><br/>"
+                        line += "<td><b>" + item.email + "</b><br/>"
+                        line +=  item.body+ "</td>"
+                        line += "</tr>";
+                    $("#tblPosts2").append(line);
+                });
+                $("#main").show();
+            })
+            .fail((xhr, status, error)=>{
+
+            })
+    }
+    function showComments(){
         $("#main").hide();
         $("#detail").show();
-        S("#comment").show();
+        $("#comment").show();
         var url = "https://jsonplaceholder.typicode.com/posts/comments"+id;
         $.getJSON(url)
             .done((data)=>{
                 console.log(data);
-                var line = "<tr id='rowcomment'>";
+                var line = "<tr id='comment'>";
                         line += "<td>"+ data.postId + "</td>";
-                        line += "<td>"+ data.id + "</td>";
-                        line += "<td>"+ data.name + "</td>";
-                        line += "<td>"+ data.email + "</td>";
-                        line += "<td>"+ data.body + "</td>";
+                        line += "<td><b>" + data.id + "</b><br/>"
+                        line += "<td><b>" + data.name + "</b><br/>"
+                        line += "<td><b>" + data.email + "</b><br/>"
+                        line += "<td><b>" + data.body + "</b><br/>"
                         line += "</tr>";
                     $("#tblComments").append(line);           
             })
@@ -113,6 +152,7 @@
 
             })
     }
+    
     $(()=>{
         loadPosts();
         $("#btnBack").click(()=>{
@@ -120,13 +160,12 @@
             $("#detail").hide();
             $("#rowdetail").remove();
         });
-    })
-    $(()=>{
-        loadComments();
+        loadPosts2();
         $("#btnComment").click(()=>{
             $("#main").show();
+            $("#main2").show();
             $("#detail").hide();
-            $("#rowcomment").remove();
+            $("#Comment").hide();   
         });
     })
 </script>
